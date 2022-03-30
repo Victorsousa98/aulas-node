53363ff5ecaac594808c39e69c67d81f64c510f4
Victorsousa98 <victorsousa708@gmail.com>
2022-03-30 15:59:05 -0300

    cria modelo de usuario com mongoose

--- mongodb/index.js
+++ mongodb/index.js
@@ -7,3 +7,30 @@ mongoose.connect('mongodb://localhost/aprendendo', {
 })
     .then(() => console.log('Conectado ao MongoDB...'))
     .catch(err => console.log('Erro ao se conectar ao MongoDB: ' + err));
+
+    
+// Model de usuário
+const UsuarioSchema = mongoose.Schema({ // criando um schema. O schema é um modelo de como os dados serão armazenados no banco de dados
+    nome: {
+        type: String, //tipo do dado
+        required: true, //campo obrigatório
+        trim: true //retira espaços em branco
+    },
+    email: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    login: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    senha: {
+        type: String,
+        required: true,
+        trim: true
+    }
+});
+
+mongoose.model('Usuarios', UsuarioSchema); // criando um modelo de usuário
