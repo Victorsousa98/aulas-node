cde74131f9af35fa850278b85d42940f541a582b
Victorsousa98 <victorsousa708@gmail.com>
2022-03-30 16:38:51 -0300

    aplicação pronta

--- mongodb/index.js
+++ mongodb/index.js
@@ -2,13 +2,11 @@ const mongoose = require('mongoose');
 
 //conetando ao banco de dados mongodb com o mongoose
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost/aprendendo', {
-    useMonogDBClient: true
-})
+mongoose.connect('mongodb://localhost/aprendendo')
     .then(() => console.log('Conectado ao MongoDB...'))
     .catch(err => console.log('Erro ao se conectar ao MongoDB: ' + err));
 
-    
+
 // Model de usuário
 const UsuarioSchema = mongoose.Schema({ // criando um schema. O schema é um modelo de como os dados serão armazenados no banco de dados
     nome: {
@@ -32,5 +30,15 @@ const UsuarioSchema = mongoose.Schema({ // criando um schema. O schema é um mod
         trim: true
     }
 });
+//Collection de usuários
+    mongoose.model('Usuarios', UsuarioSchema); // criando um modelo de usuário
 
-mongoose.model('Usuarios', UsuarioSchema); // criando um modelo de usuário
+//crianção de um usuário
+const AdicionaDados = mongoose.model('Usuarios');
+    new AdicionaDados({
+        nome: 'João',
+        email: 'victorsousa@email.com',
+        login: 'victorsousa',
+        senha: '123456'
+    }).save().then(() => console.log('Usuário criado com sucesso!'))
+        .catch(err => console.log('Erro ao criar usuário: ' + err));
